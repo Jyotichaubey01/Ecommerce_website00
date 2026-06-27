d1c8664301f1fd9fe4eeb314137ff66606ada46d
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-06-27 16:35:21 +0530

    commit05

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
+import NewsletterBox from '../components/NewsletterBox'
+import Footer from '../components/Footer'
 
 const Home = () => {
     return (
@@ -13,6 +15,9 @@ const Home = () => {
             <LatestCollection/>
             <BestSeller/>
             <OurPolicy/>
+            <NewsletterBox/>
+            <Footer/>
+
             </div>
 
     )
